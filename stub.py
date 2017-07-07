306c6220a2f8f1d21e3bed3935f8e34a6cbbb4c2
JPTIZ <jpaulotiz@gmail.com>
2017-07-06 21:34:46 -0300

    Use some of cfg.py methods. Test-stub grammars as samples.

--- stub.py
+++ stub.py
@@ -1,35 +1,68 @@
+from cfg import CFG
+
+
 def grammar_from(text):
-    return Grammar()
+    if text == '5':
+        return CFG.create(initial_symbol='S',
+                          productions={'S': {'a S a', 'b S b', 'a', 'b'}})
+    if text == '7c':
+        return CFG.create(initial_symbol='S',
+                          productions={'S': {'a A b S', 'aS', 'c'},
+                                       'A': {'a A b A', 'c'},
+                                      })
+    if text == '7d':
+        return CFG.create(initial_symbol='S',
+                          productions={'S': {'L a R', 'R'},
+                                       'L': {'b R', 'i'},
+                                       'R': {'L'},
+                                      })
+    if text == '7e':
+        return CFG.create(initial_symbol='S',
+                          productions={'S': {'A a A b', 'B b B a'},
+                                       'A': {'&'},
+                                       'B': {'&'},
+                                      })
+    return CFG.create(initial_symbol='S',
+                      productions={'S': {'&'}})
+
+def non_terminals(grammar):
+    return grammar.nonterminals
 
 def as_proper(grammar):
-    return grammar.make_proper()
+    return grammar
 
 def accept(grammar, text):
-    return text in {'a', 'b', 'aab', 'aba', 'baaa'}
+    try:
+        *_, result = grammar.parse(text)
+    except ValueError as e:
+        print(f'Rejected. Reason: {e}')
+        return False
+    return True
 
 def first(grammar):
-    return grammar._first_()
+    return {s: grammar.first(s) for s in grammar.nonterminals}
 
 
 def follow(grammar):
-    return grammar._follow_()
+    return {s: grammar.follow(s) for s in grammar.nonterminals}
 
 
 def first_nt(grammar):
-    return grammar._first_nt_()
+    return {s: grammar.first_nonterminal(s) for s in grammar.nonterminals}
 
 
 def build_parse_table(grammar):
-    return grammar._parse_table_()
+    return grammar.parse_table()
 
 
 class Grammar:
+    def __init__(self):
+        self.nonterminals = {'S', 'A'}
+
+
     '''Stub grammar.'''
     def make_proper(self):
         return self
-
-    def non_terminals(self):
-        return {'S', 'A'}
 
     def _first_(self):
         return {'S': {'a', 'b'},
